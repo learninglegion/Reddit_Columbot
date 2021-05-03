fd6752e99ae9e74f8a4553309cb25d968f81a64e
learninglegion <brent.beckwith@gmail.com>
2021-05-03 16:27:31 -0600

    comment count

--- columbot.py
+++ columbot.py
@@ -7,18 +7,21 @@ reddit = praw.Reddit('bot1')
 #subreddit = reddit.subreddit("pythonforengineers")
 
 #GET USERNAME TO INVESTIGATE (i.e. uname2i) AND CREATE suspect INSTANCE
-uname2i = input("Give me a username to investigate: ")
-suspect = reddit.redditor(uname2i)
+#uname2i = input("Give me a username to investigate: ")
+#suspect = reddit.redditor(uname2i)
+
+#testing
+suspect = reddit.redditor('utvillans')
 
 #GET NUMBER OF SUBMISSIONS/POSTS (postcounter)
 postcounter = 0
-submissions = suspect.submissions.new(limit=5)
+submissions = suspect.submissions.new(limit=10)
 for submission in submissions:
     postcounter += 1
 
 #GET NUMBER OF COMMENTS (commentcounter)
 commentcounter = 0
-for comment in suspect.comments.new(limit=5):
+for comment in suspect.comments.new(limit=10):
     commentcounter += 1
 
 #PREVIOUS COMMENT COUNTER - GETS COMMENT TEXT
@@ -30,14 +33,20 @@ for comment in suspect.comments.new(limit=5):
 
 #GET SUSPECT COMMENT SUBREDDIT (suscomsubreds) COUNTS
 suscomsubreds = []
-for comment in suspect.comments.new(limit=None):
+for comment in suspect.comments.new(limit=10):
     suscomsubreds.append(comment.subreddit.display_name)
 counted_subreds = Counter(suscomsubreds)
-top5subreds = counted_subreds.most_common(5)
-print(top5subreds)
+#top5subreds = counted_subreds.most_common(5)
+
+#feedback
+#print(f'suscomsubreds: {suscomsubreds}')
+#print(f'countedsubreds: {counted_subreds}')
+#print(f'top5subreds: {top5subreds}')
 
 #PRINT RESULTS
 print(f"{suspect} has {suspect.link_karma} karma.")
 print(f"{suspect} has {postcounter} submissions.")
 print(f"{suspect} has {commentcounter} comments.")
-print(f"{suspect} posts most frequently to {top5subreds}.")
+print(f"{suspect}'s five most frequently commented subreddits:")
+for subreddit, count in counted_subreds.most_common(5):
+    print(f"{subreddit}: {count} posts")
